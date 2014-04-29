ea61e017b4db42351f41dede4791fdb61bca9ae1
Matthew Smart <matthew.smart@thescore.com>
2014-04-29 16:28:20 -0400

    Add Borkfile and tests

---
+++ spec/spec_helper.rb
@@ -0,0 +1,7 @@
+RSpec.configure do |config|
+  # Use color in STDOUT
+  config.color_enabled = true
+
+  # Use the specified formatter
+  config.formatter = :documentation # :progress, :html, :textmate
+end
