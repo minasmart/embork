165728dea6519f78b73fa90b9cfd8067c42a89e1
Matthew Smart <m.d.smart@gmail.com>
2014-05-12 08:39:39 -0400

    Barber, work in progress

---
+++ spec/embork/sprockets/ember_handlebars_compiler/cjs_template.js
@@ -0,0 +1,20 @@
+"use strict";
+var template = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
+  var buffer = '', stack1, escapeExpression=this.escapeExpression;
+
+
+  data.buffer.push("<p>\n  Hello ");
+  stack1 = helpers._triageMustache.call(depth0, "name", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push(", here is a horrible picure:\n  <br />\n  <img ");
+  data.buffer.push(escapeExpression(helpers['bind-attr'].call(depth0, {hash:{
+    'src': ("horriblePictureUrl")
+  },hashTypes:{'src': "ID"},hashContexts:{'src': depth0},contexts:[],types:[],data:data})));
+  data.buffer.push(" />\n</p>\n");
+  return buffer;
+  
+});
+
+exports["default"] = template;
--- spec/embork/sprockets/ember_handlebars_compiler_spec.rb
+++ spec/embork/sprockets/ember_handlebars_compiler_spec.rb
@@ -2,8 +2,13 @@ require 'spec_helper'
 
 require 'embork/sprockets/ember_handlebars_compiler'
 
+require 'pry'
+
+
 describe 'Embork::Sprockets::EmberHandlebarsCompiler' do
-  let(:root_path) { File.expand_path '../ember_handlebars_compiler', __FILE__ }
+  let(:root_path) { File.expand_path('../ember_handlebars_compiler', __FILE__) }
+  let(:global_hbs_specimen) { File.read(File.join(root_path, 'global_hbs_template.js')).strip }
+  let(:global_handlebars_specimen) { File.read(File.join(root_path, 'global_handlebars_template.js')).strip }
 
   let(:app) do
     s = Sprockets::Environment.new root_path
@@ -15,15 +20,41 @@ describe 'Embork::Sprockets::EmberHandlebarsCompiler' do
     end
   end
 
-  it "compiles handlebars source to ember handlebars" do
+  it 'compiles handlebars source to ember handlebars' do
     get '/my_handlebars_template.js'
     expect(last_response).to be_ok
-    expect(last_response.body).to eq("")
+    expect(last_response.body).to eq(global_handlebars_specimen)
   end
 
-  it "compiles hbs source to ember handlebars" do
+  it 'compiles hbs source to ember handlebars' do
     get '/my_hbs_template.js'
     expect(last_response).to be_ok
-    expect(last_response.body).to eq("")
+    expect(last_response.body).to eq(global_hbs_specimen)
+  end
+
+  context 'CommonJS' do
+    before(:all) { Embork::Sprockets::EmberHandlebarsCompiler.compile_to = :cjs }
+    after(:all) { Embork::Sprockets::EmberHandlebarsCompiler.compile_to = :globals }
+
+    let(:cjs_specimen) { File.read(File.join(root_path, 'cjs_template.js')).strip }
+
+    it 'compiles hbs source to ember handlebars' do
+      get '/my_hbs_template.js'
+      expect(last_response).to be_ok
+      expect(last_response.body).to eq(cjs_specimen)
+    end
+  end
+
+  context 'AMD' do
+    before(:all) { Embork::Sprockets::EmberHandlebarsCompiler.compile_to = :amd }
+    after(:all) { Embork::Sprockets::EmberHandlebarsCompiler.compile_to = :globals }
+
+    let(:amd_specimen) { File.read(File.join(root_path, 'amd_template.js')).strip }
+
+    it 'compiles hbs source to ember handlebars' do
+      get '/my_hbs_template.js'
+      expect(last_response).to be_ok
+      expect(last_response.body).to eq(amd_specimen)
+    end
   end
 end
