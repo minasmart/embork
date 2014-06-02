6ea06a9ff6ebe995ac6a8810c905cfd29c66f3bf
Matthew Smart <matthew.smart@thescore.com>
2014-06-02 14:02:07 -0400

    Add loader and app to initial config.

--- blueprint/config/production/application.js
+++ blueprint/config/production/application.js
@@ -1,9 +1,11 @@
 //= require jquery/dist/jquery.min
 //= require handlebars/handlebars.runtime.min
 //= require ember/ember.prod
+//= require loader/loader
 //= require ic-ajax/dist/named-amd/main
 //= require ember-data/ember-data.prod
 //= require ember-cli-shims/app-shims
 //= require ember-load-initializers/ember-load-initializers
+//= require_tree ../../app
 
 window.ENV = {};
